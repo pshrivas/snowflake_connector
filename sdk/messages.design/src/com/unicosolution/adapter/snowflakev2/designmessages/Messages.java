2c2ebf311f2e61bc0d43b931ff269c1aa5fa5085
Shige Takeda <smtakeda@gmail.com>
2016-09-02 17:58:27 +0000

    Updated the messages. Don't update Message.java directly. It is generated from designtime.xml/runtime.xml

--- sdk/messages.design/src/com/unicosolution/adapter/snowflakev2/designmessages/Messages.java
+++ sdk/messages.design/src/com/unicosolution/adapter/snowflakev2/designmessages/Messages.java
@@ -1,25 +1,24 @@
 package com.unicosolution.adapter.snowflakev2.designmessages;
 
-public class Messages {
 
-	public static final String BUNDLE_ID = "com.unicosolution.adapter.snowflakev2.messages.design.SnowflakeV2designtime";
+public class Messages  {
 
-	public static final String SnowflakeV2_CONN_SUCC_200 = "SnowflakeV2_CONN_SUCC_200";
+    public static final String BUNDLE_ID = "com.unicosolution.adapter.snowflakev2.messages.design.SnowflakeV2designtime";
 
-	public static final String SNOWFLAKE_CONN_ERROR = "Connection properties cannot be null or empty";
-	
-    public static final String INVALID_PASSCODE_905 = "INVALID_PASSCODE";
-    
-    public static final String EMPTY_USER_900 = "EMPTY_USERNAME";
-    
-    public static final String EMPTY_PASSWORD_901 = "EMPTY_PASSWORD";
-    
-    public static final String EMPTY_DB_902 = "EMPTY_DB";
-    
-    public static final String EMPTY_SCHEMA_903 = "EMPTY_SCHEMA";
-    
-    public static final String EMPTY_WAREHOUSE_904 = "EMPTY_WAREHOUSE";
-    
-    public static final String EMPTY_ACCOUNT_905 = "EMPTY_ACCOUNT";
+    public static final String SnowflakeV2_EMPTY_PASSWORD_901 = "SnowflakeV2_EMPTY_PASSWORD_901";
+
+    public static final String SnowflakeV2_CONN_SUCC_200 = "SnowflakeV2_CONN_SUCC_200";
+
+    public static final String SnowflakeV2_EMPTY_ACCOUNT_905 = "SnowflakeV2_EMPTY_ACCOUNT_905";
+
+    public static final String SnowflakeV2_CONN_ERROR_201 = "SnowflakeV2_CONN_ERROR_201";
+
+    public static final String SnowflakeV2_EMPTY_SCHEMA_903 = "SnowflakeV2_EMPTY_SCHEMA_903";
+
+    public static final String SnowflakeV2_EMPTY_WAREHOUSE_904 = "SnowflakeV2_EMPTY_WAREHOUSE_904";
+
+    public static final String SnowflakeV2_EMPTY_DB_902 = "SnowflakeV2_EMPTY_DB_902";
+
+    public static final String SnowflakeV2_EMPTY_USER_900 = "SnowflakeV2_EMPTY_USER_900";
 
 }
